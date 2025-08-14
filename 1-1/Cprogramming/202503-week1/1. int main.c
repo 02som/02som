6e6bee0445245b2837cc08531300f8c67c2adc35
02som <116708024+02som@users.noreply.github.com>
2025-08-14 22:24:23 +0900

    Delete 1-1/Cprogramming directory

--- 1-1/Cprogramming/202503-week1/1. int main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> //헤더파일 포함, 입출력 라이브러리
-int main(void) //main()함수: c프로그램 시작(진입점 함수)
-{
-	printf("Hello, World! \n"); //hello world 문자열 출력
-	printf("Good Job! \n"); 
-	return 0;
-
-	//void main(void)의 경우 return 불필요, int 쓸 경우 return 필요
-}
