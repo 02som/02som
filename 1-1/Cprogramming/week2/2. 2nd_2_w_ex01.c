d90780d517ed64389236784a54a24a2c959112fb
02som <116708024+02som@users.noreply.github.com>
2025-08-14 21:51:54 +0900

    Create 2. 2nd_2_w_ex01.c

---
+++ 1-1/Cprogramming/week2/2. 2nd_2_w_ex01.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+int main(void)
+{
+	printf("학과: 인공지능학부 \n");
+	printf("학번: 20251234 \n");
+	printf("이름: 이소민 \n");
+	//혹은
+	printf("학과: 인공지능학부 \n학번: 20251234 \n이름: 이소민 \n");
+	return 0;
+
+}
