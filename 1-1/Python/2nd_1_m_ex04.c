3cc07fde7b487dba4e5c9fc76fb4dd8a186ff4b6
02som <116708024+02som@users.noreply.github.com>
2026-03-10 17:27:35 +0900

    Add files via upload

---
+++ 1-1/Python/2nd_1_m_ex04.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+int main()
+{
+    char dept[100];
+    int s_id;
+    char name[20] = "홍길동";
+
+    printf("학과를 입력하세요: ");
+    scanf("%s", dept);
+    printf("학번을 입력하세요: ");
+    scanf("%d", &s_id);
+
+    printf("저는 %s학과에 재학중인 %d학번 %s입니다.", dept, s_id, name);
+    return 0;
+}*/
